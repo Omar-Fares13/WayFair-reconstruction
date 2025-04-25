44f47c8e9e2b887ca2dd510c00b0d44f581d8651
Omar-Fares13 <omarfares820214@gmail.com>
2025-04-26 01:38:28 +0300

    Authentication Base were added

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import shop_view, product_detail_view
+from .views import shop_view, product_detail_view,shop_by_category
 
 urlpatterns = [
     path('shop/', shop_view, name='shop'),
     path('product/<int:pk>/', product_detail_view, name='product_detail'),
+    path('shop/category/<slug:category_slug>/', shop_by_category, name='shop_by_category'),
 ]
--- products/views.py
+++ products/views.py
@@ -2,11 +2,20 @@ from django.core.paginator import Paginator
 from django.shortcuts import render
 from .models import Product, Category
 from django.shortcuts import get_object_or_404
+from django.db.models import Q
 
 def shop_view(request):
-    products = Product.objects.all()
+
+    query = request.GET.get('q')
+    products = Product.objects.filter(is_available=True)
     categories = Category.objects.all()
 
+    if query:
+        products = products.filter(
+            Q(title__icontains=query) | Q(description__icontains=query)
+        )
+
+    products = products.order_by('-created_at')
     paginator = Paginator(products, 9)  # 9 per page
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
@@ -16,14 +25,9 @@ def shop_view(request):
         'page_obj': page_obj,
         'products': page_obj.object_list,
         'total_products': paginator.count,
+        'query': query,
     }
     return render(request, 'products/shop.html', context)
-
-
-def product_detail_view(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    return render(request, 'products/product_detail.html', {'product': product})
-
 
 def product_detail_view(request, pk):
     product = get_object_or_404(Product, pk=pk)
@@ -32,3 +36,20 @@ def product_detail_view(request, pk):
         'product': product,
         'similar_products': similar_products,
     })
+
+def shop_by_category(request, category_slug):
+    category = get_object_or_404(Category, slug=category_slug)
+    products = Product.objects.filter(category=category, is_available=True).order_by('-created_at')
+    categories = Category.objects.all()
+    paginator = Paginator(products, 8)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        'products': page_obj,
+        'page_obj': page_obj,
+        'categories': categories,
+        'total_products': products.count(),
+        'selected_category': category,
+    }
+    return render(request, 'products/shop.html', context)
